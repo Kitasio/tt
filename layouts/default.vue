7413062692c9cadfd99dc185b5e8c25636a7cb14
Kitasio <56487191+Kitasio@users.noreply.github.com>
2021-03-04 18:56:53 +0200

    aasdasd

--- layouts/default.vue
+++ layouts/default.vue
@@ -31,6 +31,14 @@ export default {
       if (date.getHours() >= 20 || date.getHours() <= 9) {
         setTimeout(() => this.$vuetify.theme.dark = true, 0)
       }
+    },
+    ChangeTheme: function() {
+      var now = new Date();
+      var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 20, 0, 0, 0) - now;
+      if (millisTill10 < 0) {
+          millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
+      }
+      setTimeout(() => this.$vuetify.theme.dark = true, millisTill10)
     }
   },
   mounted() {
