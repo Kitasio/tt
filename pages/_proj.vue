817fac398964270ee840c05c84543a94c3f3b024
Kitasio <56487191+Kitasio@users.noreply.github.com>
2021-03-06 21:54:23 +0200

    fixed project

--- pages/_proj.vue
+++ pages/_proj.vue
@@ -1,7 +1,9 @@
 <template>
   <v-main>
     <div>
-      <nuxt-content :document="project" />
+      <div style="height: 100vh;" v-for="i in project" :key="i.slug">
+        <iframe :src="i.link" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" width="100%" height="800px" frameborder="0" seamless></iframe>
+      </div>
     </div>
   </v-main>
 </template>
